a0f6061d90435132ed2c42aa3d0ad634a4d2379b
Fábio Augusto Rodrigues <69022068+FabioAugustoRodrigues@users.noreply.github.com>
2022-11-20 19:24:27 -0300

    Criando página para assistir os vídeos de um curso

--- js/visualizar_curso.js
+++ js/visualizar_curso.js
@@ -1,3 +1,6 @@
+let objetoDosVideosCarregados;
+let arrayDosVideosCarregados;
+
 function carregaCurso(id) {
     get("https://hackjoy-api.herokuapp.com/courses/" + id, {}, function (data, textStatus, xhr) {
         $("#imagem_do_curso").attr("src", "data:image/png;charset=utf-8;base64," + data["image"]);
@@ -7,9 +10,54 @@ function carregaCurso(id) {
     }, true);
 }
 
+function carregaVideo(id) {
+
+    get("https://hackjoy-api.herokuapp.com/contents/organized/course/" + id, {}, function (data, textStatus, xhr) {
+        objetoDosVideosCarregados = data;
+        arrayDosVideosCarregados = [];
+
+        if (Array.isArray(objetoDosVideosCarregados) && objetoDosVideosCarregados.length == 0) {
+            $("#listaDeVideosCadastrados tbody").append(
+                "<p class='titulos'>Não possui video cadastrado</p>"
+            );
+
+            objetoDosVideosCarregados = null;
+        } else {
+            let i = 0;
+            let urlDoPrimeiroVideo = "";
+            let nomeDoPrimeiroVideo = "";
+
+            for (const indice in objetoDosVideosCarregados) {
+                arrayDosVideosCarregados[i] = objetoDosVideosCarregados[indice]["id"];
+                if (urlDoPrimeiroVideo == "") {
+                    urlDoPrimeiroVideo = objetoDosVideosCarregados[indice]["link"];
+                    nomeDoPrimeiroVideo = objetoDosVideosCarregados[indice]["name"];
+                }
+
+                $("#listaDeVideosCadastrados tbody").append(
+                    "<tr>" +
+                    "<td class='assistirVideo' urlDoVideo='" + objetoDosVideosCarregados[indice]["link"] + "' nomeDoVideo='" + objetoDosVideosCarregados[indice]["name"] + "'>" +
+                    "<i class='fs-4 bi-play'></i>" +
+                    objetoDosVideosCarregados[indice]["name"] +
+                    "</td>" +
+                    "</tr>"
+                );
+
+                i++;
+            }
+
+            $("td.assistirVideo:first").addClass("videoSelecionado");
+            $("#iframeDoVideoAtual").attr("src", urlDoPrimeiroVideo);
+            $("#nome_do_conteudo_atual").text(nomeDoPrimeiroVideo);
+        }
+
+    })
+}
+
 $(document).ready(() => {
 
     carregaCurso(getParametroPorChave("id"));
+    carregaVideo(getParametroPorChave("id"));
 
     $("#adquirir_curso").on("click", function () {
         $("#adquirir_curso").attr("disabled", "disabled");
@@ -21,6 +69,19 @@ $(document).ready(() => {
             $("#adquirir_curso").removeAttr("disabled");
             $("#carregaAdquirirCurso").hide();
         }, true);
+    });
+
+    $(document).on("click", ".assistirVideo", function () {
+        $(".videoSelecionado").removeClass("videoSelecionado");
+        $(this).addClass("videoSelecionado");
+
+        let urlDoVideoSelecionado = $(this).attr("urlDoVideo");
+        let nomeDoVideoSelecionado = $(this).attr("nomeDoVideo");
+
+        $("#iframeDoVideoAtual").attr("src", urlDoVideoSelecionado);
+        $("#nome_do_conteudo_atual").text(nomeDoVideoSelecionado);
+
+        window.scrollTo(0, 0);
     });
 
 })
